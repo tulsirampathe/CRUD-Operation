f505a49fc5ef133d460fff20af613533ef5850aa
tulsirampathe <tulsirampathe81@gmail.com>
2024-05-29 14:13:56 +0530

    crud operatin

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/textapp1")
+
+const userSchema = mongoose.Schema({
+    image : String,
+    email: String,
+    name: String
+})
+
+module.exports =  mongoose.model('user', userSchema)
